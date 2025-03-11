26089caf62a071a9af855a122528477f070fa1cd
OblakoEcarlate <oblako.soleil@gmail.com>
2025-03-11 10:43:35 +0100

    Blog advanced

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,55 @@
+class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+  end
+
+  def show
+  end
+
+  def new
+      @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post, notice: 'Blog post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post, notice: 'Blog post was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path, notice: 'Blog post was successfully destroyed.'
+  end
+
+  private
+  def blog_post_params
+    params.required(:blog_post).permit(:title, :content, :published_at)
+  end
+
+  def set_blog_post
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Blog Post not found."
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "You need to sign in or sign up before continuing." unless user_signed_in?
+  end
+end
---
+++ app/models/blog_post.rb
@@ -0,0 +1,26 @@
+class BlogPost < ApplicationRecord
+  has_rich_text :content
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  # version ancienne pour les requetes sql
+  # scope :sorted, -> { order(Arel.sql("published_at DESC NULLS LAST").order(updated_at: :desc)) }
+  # version nouvelle
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(published_at: :desc) }
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
+end
